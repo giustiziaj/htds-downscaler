283bb93d716864f051af866d8297a591a4c6ed90
Joe Giustizia <joseph.giustizia@canada.ca>
2020-04-15 15:53:37 -0400

    updated gitignore, added documentation

--- scaler.py
+++ scaler.py
@@ -4,22 +4,22 @@ from types import FunctionType
 from wand.image import Image
 
 class Scaler:
-  def __init__(
-      self,
-      factor: FunctionType or Number = 1):
+  def __init__(self, factor: FunctionType or Number = 1):
+    """
+    factor can be either a function (on int) to which converts initial
+    dimensions to new dimensions, or a scalar factor for the dimensions.
+    """
 
     if isinstance(factor, Number):
       self.func = lambda x: int(x * factor)
     elif isinstance(factor, FunctionType):
       self.func = lambda x: int(factor(x))
 
-  def new_dimensions(self, img: Image) -> tuple:
+  def new_dimensions(self, img: Image) -> (int, int):
     return self.func(img.width), self.func(img.height)
 
-  def scale(
-      self,
-      infile: str,
-      outfile: str):
+  def scale(self, infile: str, outfile: str):
+    """ rescales `infile` and writes new image to `outfile` """
 
     with Image(filename=infile) as src:
       width, height = self.new_dimensions(src)
